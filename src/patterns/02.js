f89ee52656634e0ab19af821fdc8c0614080af4e
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-08-01 14:07:54 -0700

    first commit

---
+++ src/patterns/02.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import "../styles/MediumClap.css";
+
+function MediumClap() {
+  return (
+    <h1>Hi clap 2</h1>
+  );
+}
+
+export default MediumClap;
---
+++ src/patterns/04.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../styles/Clap.css";
+
+function Clap() {
+  return (
+    <h1>
+      Hello, 4 Component!
+    </h1>
+  );
+}
+
+export default Clap;
